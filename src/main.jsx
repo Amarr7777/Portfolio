50eefc580f3668657894046f35c0fce18a269c08
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-04-24 10:24:04 +0530

    tailwind configered

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <div>Hello World</div>
+    <div className='font-bold text-red-800'>Hello World</div>
   </React.StrictMode>,
 )
