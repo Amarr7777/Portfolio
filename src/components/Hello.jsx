8792d003a507b6fe9b610be9222ba51468386739
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-04-24 10:31:27 +0530

    File re-structured

---
+++ src/components/Hello.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Hello() {
+  return (
+    <div className='font-bold text-red-800 flex felx-1 justify-center items-center bg-black h-[100vh]'>Hello World</div>
+  )
+}
+
+export default Hello
