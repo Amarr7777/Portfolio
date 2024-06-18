236761a5768ced264aa1c7ae801284736bf2ce71
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-06-18 08:12:39 +0530

    skills scroll bar fixed

--- src/components/SectionONe.jsx
+++ src/components/SectionONe.jsx
@@ -1,20 +1,40 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import imgSrc from "../assets/back1.jpg";
 import Home from "./Home";
 import Skills from "./Skills";
 import Projects from "./Projects";
-import ProjectMobile from "./ProjectMobile";
 import SideBar from "./SideBar";
 
 function SectionOne() {
   const [pageIndex, setPageIndex] = useState(0);
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const windowHeight = window.innerHeight;
+      if (scrollPosition < windowHeight) {
+        setPageIndex(0);
+      } else if (
+        scrollPosition >= windowHeight &&
+        scrollPosition < 2 * windowHeight
+      ) {
+        setPageIndex(1);
+      } else if (scrollPosition >= 2 * windowHeight) {
+        setPageIndex(2);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <div className="relative h-screen w-full">
       <Header pageIndex={pageIndex} />
       {pageIndex === 0 && <Home />}
-      {pageIndex === 1 && <Skills/>}
+      {pageIndex === 1 && <Skills />}
       {pageIndex === 2 && <Projects />}
       <SideBar setPageIndex={setPageIndex} pageIndex={pageIndex} />
       <Footer setPageIndex={setPageIndex} pageIndex={pageIndex} />
