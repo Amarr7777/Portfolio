4def47e9575ad20686257379d2ed7785f2e341c3
Amar G Nath <ambadypradeepcoc@gmail.com>
2024-09-07 14:55:33 +0530

    scroll issue fixed

--- src/components/SectionONe.jsx
+++ src/components/SectionONe.jsx
@@ -8,28 +8,6 @@ import SideBar from "./SideBar";
 
 function SectionOne() {
   const [pageIndex, setPageIndex] = useState(0);
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollPosition = window.scrollY;
-      const windowHeight = window.innerHeight;
-      if (scrollPosition < windowHeight) {
-        setPageIndex(0);
-      } else if (
-        scrollPosition >= windowHeight &&
-        scrollPosition < 2 * windowHeight
-      ) {
-        setPageIndex(1);
-      } else if (scrollPosition >= 2 * windowHeight) {
-        setPageIndex(2);
-      }
-    };
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
   return (
     <div className="relative h-screen w-full">
       <Header pageIndex={pageIndex} setPageIndex={setPageIndex} />
